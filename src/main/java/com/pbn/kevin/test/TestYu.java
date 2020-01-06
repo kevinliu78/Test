21aca539e89969049fae761ecfc4d239b169768b
kevin <liuweshuai@163.com>
2020-01-06 16:49:16 +0800

    add testyu

---
+++ src/main/java/com/pbn/kevin/test/TestYu.java
@@ -0,0 +1,30 @@
+package com.pbn.kevin.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: LWS
+ * @Date: 2019/12/26 15:03
+ */
+public class TestYu {
+    public static void main(String[] args) {
+        boolean result = false;
+        List<Boolean> list = new ArrayList<>();
+        list.add(true);
+        list.add(true);
+        list.add(true);
+        for (int i = 0; i < list.size(); i++) {
+            if (i == 0) {
+                result = list.get(i);
+            } else {
+                result = result && list.get(i);
+            }
+            System.out.println(result);
+            if (!result) {
+                break;
+            }
+        }
+        System.out.println("result=============="+result);
+    }
+}
