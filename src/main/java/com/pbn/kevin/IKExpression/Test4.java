5a5d734cf9d7ec70b111bae55afb3fb58553140f
kevin <liuweshuai@163.com>
2020-08-28 17:17:11 +0800

    add

---
+++ src/main/java/com/pbn/kevin/IKExpression/Test4.java
@@ -0,0 +1,42 @@
+package com.pbn.kevin.IKExpression;
+
+import org.wltea.expression.ExpressionEvaluator;
+import org.wltea.expression.PreparedExpression;
+import org.wltea.expression.datameta.Variable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: LWS
+ * @Date: 2020/3/5 14:33
+ */
+public class Test4 {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0){
+            args = new String[1];
+            args[0] = "IK Expression V2.0.5";
+        }
+
+        //定义表达式
+        String expression = "$问好(name)";
+        //给表达式中的[版本]赋上下文值
+        List<Variable> variables = new ArrayList<>();
+        variables.add(Variable.createVariable("name",args[0]));
+
+        //预编译表达式
+        PreparedExpression pe = ExpressionEvaluator.preparedCompile(expression, variables);
+
+        //执行表达式
+        Object result = pe.execute();
+        System.out.println("Result = " + result);
+
+        //更改参数，再次执行预编译式
+        pe.setArgument("name","aaa");
+        result = pe.execute();
+        System.out.println("Result = " + result);
+    }
+
+}
---
+++ src/main/java/com/pbn/kevin/test/test111/Dee.java
@@ -0,0 +1,10 @@
+package com.pbn.kevin.test.test111;
+
+/**
+ * @Author: LWS
+ * @Date: 2020/4/16 19:02
+ */
+public class Dee {
+
+    private String ddddd;
+}
