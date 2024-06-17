9e43299d2d720c74f8140694090b763dc2eb7f84
kevin <liuweshuai@163.com>
2024-06-17 15:21:20 +0800

    test idea push

---
+++ src/main/java/com/kevin/TestPush.java
@@ -0,0 +1,13 @@
+package com.kevin;
+
+/**
+ * @Program: Test
+ * @Description:
+ * @Author: Liuws
+ * @Date: 2024-06-17 15:20:09
+ **/
+public class TestPush {
+    public static void main(String[] args) {
+        System.out.println("push success!!!");
+    }
+}
